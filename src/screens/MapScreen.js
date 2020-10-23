2e0bcf8446b492a82f3fb2f66293322da725415e
Lucas Rocha <lucas.almeida.r@gmail.com>
2020-10-23 13:41:07 -0300

    add 3 Filters at the bottom of MapScreen

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,29 +1,40 @@
 import React, { useContext } from 'react';
-import { Text, StyleSheet, Platform } from 'react-native';
+import { Text, StyleSheet, Platform, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import FilterItem from '../components/FilterItem';
 import Filter from '../components/Filter';
-import { moderateScale, screenHeight, screenWidth } from '../utils/scaling';
+import { screenHeight, screenWidth } from '../utils/scaling';
+import { Sizes } from '../styles';
 
 const MapScreen = ({ navigation }) => {
 
   return (
-    <SafeAreaView>
-      <Text style={{ fontSize: 30 }}>MapScreen</Text>
-      <Text>{screenHeight}</Text>
-      <Text>{screenWidth}</Text>
-      <FilterItem />
-      <Text>aaaaaaaaaaaaaaa</Text>
-      <Filter />
+    <SafeAreaView style={styles.screenContainer}>
+      <View>
+        <Text style={{ fontSize: 30 }}>MapScreen</Text>
+        <Text>{screenHeight}</Text>
+        <Text>{screenWidth}</Text>
+      </View>
+      <View style={styles.filtersContainer}>
+          <Filter />
+          <Filter />
+          <Filter />  
+      </View>
     </SafeAreaView>
   );
 }
 
-// TESTAR O FILTER ITEM, VER SE O WEB CONSEGUE EXIBIR CORRETAMENTE
-
-
 const styles = StyleSheet.create({
-  
+  screenContainer: {
+    flex: 1,
+    backgroundColor: '#ddd',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  filtersContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: Sizes.BASE_SPACING,
+  },
 });
 
 export default MapScreen;
