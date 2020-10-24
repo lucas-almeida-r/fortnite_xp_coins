dede34c8d91578fbf5bfc1372a679ebe79600503
Lucas Rocha <lucas.almeida.r@gmail.com>
2020-10-24 20:27:37 -0300

    new AsyncStorage, add coinsStatus to cache, AsyncStorage now seems to work on the web

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import MapScreen from './src/screens/MapScreen';
 import SplashScreen from './src/screens/SplashScreen';
 import { Provider as MapProvider, Context as MapContext } from './src/context/MapContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -10,6 +11,7 @@ const App = () => {
 
   useEffect(() => {
     getCachedData();
+    //AsyncStorage.clear();
     setIsLoading(false);
   }, []);
 
--- src/context/MapContext.js
+++ src/context/MapContext.js
@@ -1,23 +1,17 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import createDataContext from './createDataContext';
 
 // REDUCER
 const mapReducer = (state, { type, payload }) => {
-  let newState = { ...state };
   switch (type) {
     case 'set_filters':
       return { ...state, filters: payload };
       
     case 'update_coin_status':
-      newState.coinsStatus = newState.coinsStatus.map( c =>
-        c.id === payload.id
-          ? { ...c, status: payload.newStatus}
-          : c
-      );
-      return newState;
+      return { ...state, coinsStatus: payload };
     
     case 'get_cached_data':
-      return { ...state, filters: payload };
+      return { ...state, filters: payload.filters, coinsStatus: payload.coinsStatus };
 
     default:
       return state;
@@ -33,63 +27,81 @@ const setFilters = (dispatch, state) => async (updatedFilters, newValue) => {
   let newFilters = { ...state.filters };
   updatedFilters.forEach( f => newFilters[f] = newValue);
   await AsyncStorage.setItem('filters', JSON.stringify(newFilters));
+
+  console.log(newFilters);
   
   dispatch({ type: 'set_filters', payload: newFilters });
 };
 
+// id: is the id of the coin to be updated
 // newStatus: 'collected' or 'notCollected'
-const updateCoinStatus = dispatch => (id, newStatus) => {
-  dispatch({ type: 'update_coin_status', payload: { id, newStatus} });
+const updateCoinStatus = (dispatch, state) => async (id, newStatus) => {
+  let newCoinsStatus = [...state.coinsStatus];
+  newCoinsStatus = newCoinsStatus.map( c=> 
+    c.id === id
+      ? { ...c, status: newStatus }
+      : c
+  );
+  await AsyncStorage.setItem('coinsStatus', JSON.stringify(newCoinsStatus));
+
+  dispatch({ type: 'update_coin_status', payload: newCoinsStatus });
 };
 
 const getCachedData = dispatch => async () => {
-  const filters = await AsyncStorage.getItem('filters');
-  console.log(filters);
-  console.log(JSON.parse(filters));
+  let filters = await AsyncStorage.getItem('filters');
+  let coinsStatus = await AsyncStorage.getItem('coinsStatus');
 
-  if(filters) {
-    dispatch({ type: 'get_cached_data', payload: JSON.parse(filters) });
-  }
+  // WARNING: this could erase all coinsStatus create by the user
+  //          the login + backup will solve the issue
+  if(!filters) filters = JSON.stringify(mapContextInitialState.filters);
+  if(!coinsStatus) coinsStatus = JSON.stringify(mapContextInitialState.coinsStatus);
+
+  dispatch({ 
+    type: 'get_cached_data',
+    payload: {
+      filters: JSON.parse(filters),
+      coinsStatus: JSON.parse(coinsStatus),
+    }   
+  });
+
 };
 
 // EXPORT
+const mapContextInitialState = { 
+  coins: [
+    { id: '1', coords: { top: 0, left: 0 }, color: 'blue', week: 'week2' }, 
+    { id: '2', coords: { top: 100, left: 100 }, color: 'green', week: 'week5' }, 
+    { id: '3', coords: { top: 200, left: 200 }, color: 'golden', week: 'week1' },
+  ],
+  coinsStatus: [
+    { id: '1', status: 'collected' }, 
+    { id: '2', status: 'notCollected' }, 
+    { id: '3', status: 'collected' },
+  ],
+  filters: {
+    week1: true,
+    week2: true,
+    week3: true,
+    week4: true,
+    week5: false,
+    week6: true,
+    week7: true,
+    week8: true,
+    week9: true,
+    week10: false,
+
+    green: true,
+    blue: true,
+    purple: false,
+    golden: true,
+
+    collected: true,
+    notCollected: false,
+  },
+};
+
 export const { Provider, Context } = createDataContext(
   mapReducer,
   { setFilters, updateCoinStatus, getCachedData },
-  { 
-    //isLoading: true,
-    // downloaded from internet
-    coins: [
-      { id: '1', coords: { top: 0, left: 0 }, color: 'blue', week: 'week2' }, 
-      { id: '2', coords: { top: 100, left: 100 }, color: 'green', week: 'week5' }, 
-      { id: '3', coords: { top: 200, left: 200 }, color: 'golden', week: 'week1' },
-    ],
-
-    // set by the user
-    coinsStatus: [
-      { id: '1', status: 'collected' }, 
-      { id: '2', status: 'notCollected' }, 
-      { id: '3', status: 'collected' },
-    ],
-    filters: {
-      week1: true,
-      week2: true,
-      week3: true,
-      week4: true,
-      week5: false,
-      week6: true,
-      week7: true,
-      week8: true,
-      week9: true,
-      week10: false,
-
-      green: true,
-      blue: true,
-      purple: false,
-      golden: true,
-
-      collected: true,
-      notCollected: false,
-    },
-  }
+  mapContextInitialState
 );
