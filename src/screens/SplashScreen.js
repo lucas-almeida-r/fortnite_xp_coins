6aaad7bcdcfe0b1c728681b506fa443c572009c5
Lucas Rocha <lucas.almeida.r@gmail.com>
2020-10-28 23:26:52 -0300

    add ActivityIndicator in SplashScreen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Image, View, StyleSheet } from 'react-native';
+import { Text, Image, View, StyleSheet, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Sizes, Colors, Typography } from '../styles';
 
@@ -8,13 +8,7 @@ const SplashScreen = () => {
   
   return (
     <SafeAreaView style={styles.screenContainer}>
-      <Image
-        source={require('../../assets/splash.png')}
-        style={styles.logo}
-      />
-      <Text>
-        Partes dos materiais utilizados são marcas comerciais e/ou obras com direitos autorais da Epic Games, Inc. Todos os direitos reservados pela Epic. Este material não é oficial e não é endossado pela Epic.
-      </Text>
+      <ActivityIndicator size='large'  color="#fff"/>
     </SafeAreaView>
   );
   
@@ -27,11 +21,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  logo: {
-    width: 100,
-    height: 100
-  },
-  warning: {},
 });
 
 export default SplashScreen;
